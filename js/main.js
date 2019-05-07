5d3efd9a6b32ccfebf117c9ec905a1a35b8b16a1
Anatoliy Novohatniy <tolikn@aol.de>
2019-05-07 17:42:54 +0200

    version 3.0

--- js/main.js
+++ js/main.js
@@ -1,5 +1,38 @@
 // Spiel starten
-var startButton = document.getElementById('start');
-startButton.click(function () {
-    startSpiel();
+document.addEventListener("DOMContentLoaded", function (event) {
+    createSpielfeld();
+    var startButton = document.getElementById('start');
+    startButton.click(function () {
+        startSpiel();
+    });
 });
+
+var snake = {
+    position: [],
+    size: 3,
+    richtung: "rechts",
+    aktiv: true
+};
+
+var futter = {
+    position: [],
+    verfuegbar: false
+};
+
+var bildpunkte = 20;
+var punkte = 0;
+var laenge = 0;
+var geschwindigkeit = 10;
+
+function createSpielfeld() {
+    var size = bildpunkte;
+    var spielfeld = document.getElementById('spielfeld');
+    var row = document.createElement('div');
+
+    for (var i = 0; i < size; i++) {
+        row.innerHTML = spielfeld;
+    }
+
+    return row;
+
+}
