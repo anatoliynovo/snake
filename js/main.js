19fa2b6d95b57158813b377a98d1af19afa699e4
Anatoliy Novohatniy <tolikn@aol.de>
2019-05-09 18:37:33 +0200

    version 3.9.2

--- js/main.js
+++ js/main.js
@@ -1,24 +1,24 @@
 // Map erstellen
-$(document).ready(function() {
-    createMap();
-    $(".start").click(function() {
-        //Button deaktivieren nach dem ersten Klick
-        $(this).prop('disabled', 'disabled');
-        //Spiel starten
-        gameStart();
-    });
+$(document).ready(function () {
+	createMap();
+	$(".start").click(function () {
+		//Button deaktivieren nach dem ersten Klick
+		$(this).prop('disabled', 'disabled');
+		//Spiel starten
+		gameStart();
+	});
 });
 
 var snake = {
-    position_x: 15,
-    position_y: 14,
-    size: 3,
-    direction: 'down'
+	position_x: 1,
+	position_y: 1,
+	size: 3,
+	direction: 'down'
 };
 
 var snack = {
-    position: [],
-    available: false
+	position: [],
+	available: false
 };
 
 var columnsMap = 30;
@@ -35,105 +35,113 @@ var mapSize = columnsMap;
 // Funktion zum Kreieren der Map
 function createMap() {
 
-    for (var i = 0; i < mapSize; i++) {
-        $('#map').append('<div class="columns" id="' + counter_column + '"></div>');
-        counter_column = counter_column + 1;
-        for (var j = 0; j < mapSize - 1; j++) {
-            counter = counter + 20;
-            counter_row = counter_row + 1;
-            $('.columns:last-child').append('<div class="rows" id="' + counter_row + '"></div>');
-            $('.rows:last-child').css('margin-left', counter + 'px');
+	for (var i = 0; i < mapSize; i++) {
+		$('#map').append('<div class="columns" id="' + counter_column + '"></div>');
+		counter_column = counter_column + 1;
+		for (var j = 0; j < mapSize - 1; j++) {
+			counter = counter + 20;
+			counter_row = counter_row + 1;
+			$('.columns:last-child').append('<div class="rows" id="' + counter_row + '"></div>');
+			$('.rows:last-child').css('margin-left', counter + 'px');
 
-            if (counter == 580) {
-                counter = 0;
-            }
+			if (counter == 580) {
+				counter = 0;
+			}
 
-            if (counter_row == 30) {
-                counter_row = 1;
-            }
+			if (counter_row == 30) {
+				counter_row = 1;
+			}
 
-        }
+		}
 
-    }
+	}
 
 }
 
 function gameProcess() {
-    setTimeout(function() {
-        moveSnake();
-    });
+	setTimeout(function () {
+		document.addEventListener("keydown", moveSnake);
+	});
 }
 
-function control() {
-
-}
 
 function moveSnake(key) {
 
+	var arrow = {
+		left: 37,
+		up: 38,
+		right: 39,
+		down: 40
+	};
 
-    // Snake Koordinaten erhalten
+	// Snake Koordinaten erhalten
+	var snake = document.querySelector('#snake_head');
+	var style = getComputedStyle(snake);
+	var snake_x_t = style.marginLeft.substring(0, 3);
+	var snake_y_t = style.marginTop.substring(0, 3);
+	var snake_x = parseInt(snake_x_t, 10);
+	var snake_y = parseInt(snake_y_t, 10);
 
-    const snake = document.querySelector('#snake_head');
-    const style = getComputedStyle(snake);
-    var snake_x_t = style.marginLeft.substring(0, 3);
-    var snake_y_t = style.marginTop.substring(0, 3);
-    var snake_x = parseInt(snake_x_t, 10);
-    var snake_y = parseInt(snake_y_t, 10);
-    console.log("uff");
+	switch (key.keyCode) {
+		case arrow.left: //left           
+			snake_x = snake_x - 20;
+			if (snake_x == -20) {
+				snake_x = 580;
+			}
+			$('#snake_head').css({
+				'margin-left': snake_x + 'px'
+			});
+			console.log(snake_x);
+			break;
+		case arrow.up: //up
+			snake_y = snake_y - 20;
+			if (snake_y == -20) {
+				snake_y = 580;
+			}
+			$('#snake_head').css({
+				'margin-top': snake_y + 'px'
+			});
+			console.log(snake_y);
+			break;
+		case arrow.right: //right
+			snake_x = snake_x + 20;
+			if (snake_x == 600) {
+				snake_x = 0;
+			}
+			$('#snake_head').css({
+				'margin-left': snake_x + 'px'
+			});
+			console.log(snake_x);
+			break;
+		case arrow.down: //down
+			snake_y = snake_y + 20;
+			if (snake_y == 600) {
+				snake_y = 0;
+			}
+			$('#snake_head').css({
+				'margin-top': snake_y + 'px'
+			});
+			console.log(snake_y);
+			break;
 
 
+	}
 
-    switch (key.keyCode) {
-        case 37: //left           
-            snake_x = snake_x - 20;
-            if (snake_x == -20) {
-                snake_x = 580;
-            }
-            $('#snake_head').css({ 'margin-left': snake_x + 'px' });
-            console.log(snake_x);
-            break;
-        case 38: //up
-            snake_y = snake_y - 20;
-            if (snake_y == -20) {
-                snake_y = 580;
-            }
-            $('#snake_head').css({ 'margin-top': snake_y + 'px' });
-            console.log(snake_y);
-            break;
-        case 39: //right
-            snake_x = snake_x + 20;
-            if (snake_x == 600) {
-                snake_x = 0;
-            }
-            $('#snake_head').css({ 'margin-left': snake_x + 'px' });
-            console.log(snake_x);
-            break;
-        case 40: //down
-            snake_y = snake_y + 20;
-            if (snake_y == 600) {
-                snake_y = 0;
-            }
-            $('#snake_head').css({ 'margin-top': snake_y + 'px' });
-            console.log(snake_y);
-            break;
-
-    }
 }
-
-
 
 // Funktion zum Start des Spiels
 function gameStart() {
-    document.addEventListener("keydown", moveSnake);
-    score = 0;
-    display_size = 0;
 
-    // Score anzeigen
-    $('#points').text(score);
-    // Länge anzeigen
-    $('#size_number').text(display_size);
-    // Head anzeigen
-    $('.columns:nth-child(' + snake.position_x + ') .rows:nth-child(' + snake.position_y + ')').append('<div id="snake_head"></div>');
-    // Spieldurchlauf starten
-    gameProcess();
+	score = 0;
+	display_size = 0;
+
+	// Score anzeigen
+	$('#points').text(score);
+	// Länge anzeigen
+	$('#size_number').text(display_size);
+	// Head anzeigen
+	$('.columns:nth-child(' + snake.position_x + ') .rows:nth-child(' + snake.position_y + ')').append('<div id="snake_head"></div>');
+	// Spieldurchlauf starten
+	gameProcess();
+
 }
