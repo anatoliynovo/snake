9799cd51f910559ab40926c99d4a78f0ee560ee5
Anatoliy Novohatniy <tolikn@aol.de>
2019-05-15 12:40:50 +0200

    version 4.2

--- js/main.js
+++ js/main.js
@@ -3,16 +3,54 @@
 $('#map').hide();
 $('span').hide();
 */
+
+// Snake
+var snake = {
+	position_x: 1,
+	position_y: 1,
+	size: 1,
+	available: false,
+	head: 'snake_head'
+};
+
+// Futter
+var snack = {
+	position: [],
+	available: false
+};
+
+var totalRows = 30; // Gesamtanzahl der Reihen
+var score = 0; // Score-Zähler
+var display_size = 0; // Länge-Zähler
+
+var counter = 0; // Zähler für Pixeln (20px)
+var counter_column = 1; // id für columns
+var counter_row = 0; // id für rows
+var mapSize = totalRows;
+
+// Tastatur Eingabe
+var key_left = 37;
+var key_up = 38;
+var key_right = 39;
+var key_down = 40;
+var key_state = [ false, false, false, false ];
+
+// Taktzyklus für Snake
+var cycleTimer;
+var speed = 300; // Geschwindigkeit des Schalangenkopfs
+
+// Pop Up anzeigen, wenn Kollision stattgefunden hat
+var popup = $('.popup');
+
+// Start-Button
+var startButton = $('.start');
+
 // Map erstellen und Tasten einweisen
 $(document).ready(function() {
 	createMap();
 	initializeKeyboard();
-	initializeEverything();
-});
-
-function initializeEverything() {
-	$('.start').text('START');
-	$('.start').click(function() {
+	startButton.text('START');
+	startButton.click(function() {
 		// Start-Button deaktivieren nach dem ersten Klick
 		$('.start').prop('disabled', true);
 		// Smooth Scrolling nach ganz unten
@@ -30,42 +68,7 @@ function initializeEverything() {
 		// Spiel starten
 		gameStart();
 	});
-}
-
-// Snake
-var snake = {
-	position_x: 1,
-	position_y: 1,
-	size: 1,
-	available: true,
-	head: 'snake_head'
-};
-
-// Futter
-var snack = {
-	position: [],
-	available: false
-};
-
-var totalRows = 30; // Gesamtanzahl der Reihen
-var score = 0; // Score-Zähler
-var display_size = 0; // Länge-Zähler
-var speed = 300; // Geschwindigkeit des Schalangenkopfs
-
-var counter = 0; // Zähler für Pixeln (20px)
-var counter_column = 1; // id für columns
-var counter_row = 0; // id für rows
-var mapSize = totalRows;
-
-// Tastatur Eingabe
-var key_left = 37;
-var key_up = 38;
-var key_right = 39;
-var key_down = 40;
-var key_state = [ false, false, false, false ];
-
-// Pop Up anzeigen, wenn Kollision stattgefunden hat
-var popup = $('.popup');
+});
 
 // Funktion zum Kreieren der Map
 function createMap() {
@@ -134,72 +137,88 @@ function initializeKeyboard() {
 
 function gameProcess() {
 	if (snake.available) {
-		moveSnake();
+		resetCycle();
+		startCycle();
+		console.log('alive: ' + snake.available);
 	}
 }
 
-function moveSnake() {
-	setInterval(function() {
-		check4Collisions();
-		// Tail vom Snake (urspünglicher Head) sichern
-		var snake_tail_x = snake.position_x;
-		var snake_tail_y = snake.position_y;
-		// Score und Länge-Zähler fangen an zu zählen, wenn einer der Tasten betätigt wurde
-		if (key_state[0] || key_state[1] || key_state[2] || key_state[3]) {
-			display_size = display_size + 1;
-			score = score + 1;
-			$('#size_number').text(display_size);
-			$('#score_number').text(score);
-		}
-		// links
-		if (key_state[0]) {
-			snake.position_x = snake.position_x - 1;
-			$('.columns:nth-child(' + snake.position_y + ') .rows:nth-child(' + snake.position_x + ')').addClass(
-				snake.head
-			);
-			$('.columns:nth-child(' + snake_tail_y + ') .rows:nth-child(' + snake_tail_x + ')').removeClass(snake.head);
-			console.log('links: ' + snake.position_x);
-		}
-
-		// oben
-		if (key_state[1]) {
-			snake.position_y = snake.position_y - 1;
-			$('.columns:nth-child(' + snake.position_y + ') .rows:nth-child(' + snake.position_x + ')').addClass(
-				snake.head
-			);
-			$('.columns:nth-child(' + snake_tail_y + ') .rows:nth-child(' + snake_tail_x + ')').removeClass(snake.head);
-			console.log('oben: ' + snake.position_y);
-		}
-
-		// rechts
-		if (key_state[2]) {
-			snake.position_x = snake.position_x + 1;
-			$('.columns:nth-child(' + snake.position_y + ') .rows:nth-child(' + snake.position_x + ')').addClass(
-				snake.head
-			);
-			$('.columns:nth-child(' + snake_tail_y + ') .rows:nth-child(' + snake_tail_x + ')').removeClass(snake.head);
-			console.log('rechts: ' + snake.position_x);
-		}
-
-		// unten
-		if (key_state[3]) {
-			snake.position_y = snake.position_y + 1;
-			$('.columns:nth-child(' + snake.position_y + ') .rows:nth-child(' + snake.position_x + ')').addClass(
-				snake.head
-			);
-			$('.columns:nth-child(' + snake_tail_y + ') .rows:nth-child(' + snake_tail_x + ')').removeClass(snake.head);
-			console.log('unten: ' + snake.position_y);
+function startCycle() {
+	cycleTimer = setInterval(function() {
+		moveSnake();
+		if (check4Collisions()) {
+			resetCycle();
+			setTimeout(function() {
+				gameOver();
+			}, 300);
 		}
 	}, speed);
 }
 
+function resetCycle() {
+	clearInterval(cycleTimer);
+}
+
+function moveSnake() {
+	// Tail vom Snake (urspünglicher Head) sichern
+	var snake_tail_x = snake.position_x;
+	var snake_tail_y = snake.position_y;
+	// Score und Länge-Zähler fangen an zu zählen, wenn einer der Tasten betätigt wurde
+	if (key_state[0] || key_state[1] || key_state[2] || key_state[3]) {
+		display_size = display_size + 1;
+		score = score + 1;
+		$('#size_number').text(display_size);
+		$('#score_number').text(score);
+	}
+	// links
+	if (key_state[0]) {
+		snake.position_x = snake.position_x - 1;
+		$('.columns:nth-child(' + snake.position_y + ') .rows:nth-child(' + snake.position_x + ')').addClass(
+			snake.head
+		);
+		$('.columns:nth-child(' + snake_tail_y + ') .rows:nth-child(' + snake_tail_x + ')').removeClass(snake.head);
+		console.log('links: ' + snake.position_x);
+		console.log('links -> key_state: ' + key_state[0]);
+	}
+
+	// oben
+	if (key_state[1]) {
+		snake.position_y = snake.position_y - 1;
+		$('.columns:nth-child(' + snake.position_y + ') .rows:nth-child(' + snake.position_x + ')').addClass(
+			snake.head
+		);
+		$('.columns:nth-child(' + snake_tail_y + ') .rows:nth-child(' + snake_tail_x + ')').removeClass(snake.head);
+		console.log('oben: ' + snake.position_y);
+		console.log('oben -> key_state: ' + key_state[1]);
+	}
+
+	// rechts
+	if (key_state[2]) {
+		snake.position_x = snake.position_x + 1;
+		$('.columns:nth-child(' + snake.position_y + ') .rows:nth-child(' + snake.position_x + ')').addClass(
+			snake.head
+		);
+		$('.columns:nth-child(' + snake_tail_y + ') .rows:nth-child(' + snake_tail_x + ')').removeClass(snake.head);
+		console.log('rechts: ' + snake.position_x);
+		console.log('rechts -> key_state: ' + key_state[2]);
+	}
+
+	// unten
+	if (key_state[3]) {
+		snake.position_y = snake.position_y + 1;
+		$('.columns:nth-child(' + snake.position_y + ') .rows:nth-child(' + snake.position_x + ')').addClass(
+			snake.head
+		);
+		$('.columns:nth-child(' + snake_tail_y + ') .rows:nth-child(' + snake_tail_x + ')').removeClass(snake.head);
+		console.log('unten: ' + snake.position_y);
+		console.log('unten -> key_state: ' + key_state[3]);
+	}
+}
+
 function check4Collisions() {
-	if (snake.position_y == 1 || snake.position_y == 31 || snake.position_x == 1 || snake.position_x == 31) {
+	if (snake.position_y == 1 || snake.position_y == 31 || snake.position_x == 1 || snake.position_x == 30) {
 		snake.available = false;
-		key_state = false;
-		setTimeout(function() {
-			gameOver();
-		}, 200);
+		return key_state;
 	}
 }
 
@@ -207,17 +226,14 @@ function gameOver() {
 	popup.show();
 	$('.rows').removeClass(snake.head);
 	// Start-Button wieder aktivieren
-	var startButton = $('.start');
 	startButton.prop('disabled', false);
 	startButton.text('RESTART');
 	startButton.click(function() {
 		popup.hide();
-		initializeEverything();
-		console.log('RESTART Button is clicked');
+		gameStart();
 	});
 }
 
-// Funktion zum Starten des Spiels
 function gameStart() {
 	// Score- und Laengezaehler auf 0
 	score = 0;
@@ -230,18 +246,21 @@ function gameStart() {
 	// Snake lebt
 	snake.available = true;
 
-	// Bewegungsrichtung am Anfang
-	// kurz warten, bis der Snake sich automatisch in die festgelegte Richtung bewegen darf
-	setTimeout(function() {
-		key_state[3] = true;
-	}, 500);
+	// Bewegungsrichtung am Anfang nach "unten" auf true setzen, alle anderen sind auf "false" gesetzt
+	key_state[0] = false;
+	key_state[1] = false;
+	key_state[2] = false;
+	key_state[3] = true;
 
 	// Score anzeigen
 	$('#score_number').text(score);
+
 	// Länge anzeigen
 	$('#size_number').text(display_size);
+
 	// Head anzeigen
-	$('.columns:nth-child(' + snake.position_x + ') .rows:nth-child(' + snake.position_y + ')').addClass(snake.head);
-	// Spieldurchlauf starten
+	$('.columns:nth-child(' + snake.position_x + ') .rows:nth-child(' + snake.position_y + ')');
+
+	// Spielloop starten
 	gameProcess();
 }
